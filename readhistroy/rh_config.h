1b327181c19949d1fc1df6a8e8261d47a5798ef5
haipo yang <yang@haipo.me>
2017-04-24 16:22:47 +0800

    add read history

---
+++ readhistroy/rh_config.h
@@ -0,0 +1,53 @@
+/*
+ * Description: 
+ *     History: yang@haipo.me, 2017/04/24, create
+ */
+
+# ifndef _RH_CONFIG_H_
+# define _RH_CONFIG_H_
+
+# include <math.h>
+# include <stdio.h>
+# include <error.h>
+# include <errno.h>
+# include <ctype.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <assert.h>
+# include <inttypes.h>
+
+# include "nw_svr.h"
+# include "nw_clt.h"
+# include "nw_job.h"
+# include "nw_timer.h"
+# include "nw_state.h"
+
+# include "ut_log.h"
+# include "ut_sds.h"
+# include "ut_cli.h"
+# include "ut_misc.h"
+# include "ut_list.h"
+# include "ut_kafka.h"
+# include "ut_signal.h"
+# include "ut_config.h"
+# include "ut_decimal.h"
+# include "ut_rpc_clt.h"
+# include "ut_rpc_svr.h"
+# include "ut_rpc_cmd.h"
+
+struct settings {
+    bool                debug;
+
+    process_cfg         process;
+    log_cfg             log;
+    rpc_svr_cfg         svr;
+    mysql_cfg           db_history;
+    int                 worker_num;
+};
+
+extern struct settings settings;
+int init_config(const char *path);
+
+# endif
+

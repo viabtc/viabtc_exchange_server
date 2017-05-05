c6eeb498ee2f26b11849e7bd416183efe9ae2d67
haipo yang <yang@haipo.me>
2017-05-05 19:25:53 +0800

    update

--- test/accesshttp/test.py
+++ test/accesshttp/test.py
@@ -10,7 +10,7 @@ def call(method, *params):
     r = requests.post('http://127.0.0.1:8080/', data=json.dumps(data))
     print r.text
 
-#call("balance.update", 2, "CNY", "deposit", 100, "10000")
+call("balance.update", 2, "CNY", "deposit", 100, "10000", {})
 #call("balance.query", 1)
 #call("balance.query", 2)
 #call("balance.history", 1, "BTC", "", 0, 0, 0, 10)
@@ -24,4 +24,5 @@ def call(method, *params):
 #call("trade.order_book", "BTCCNY", 2, 0, 100)
 #call("trade.finished_order_detail", 11)
 #call("trade.finished_order_detail", 12)
-call("market.deals", "BTCCNY", 10, 5)
+#call("market.deals", "BTCCNY", 10, 5)
+call("trade.put_limit", 2, "BTCCNY", 2, "1", "5600", "0.001", "0.001")
